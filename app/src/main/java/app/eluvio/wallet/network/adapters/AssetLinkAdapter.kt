92fee5040ce2d8b2fae4219cb7611f5bb698f5f7
Stav Raviv <stav.raviv@eluv.io>
2024-10-09 16:38:21 -0700

    Fix loading images with weird path/file names

--- app/src/main/java/app/eluvio/wallet/network/adapters/AssetLinkAdapter.kt
+++ app/src/main/java/app/eluvio/wallet/network/adapters/AssetLinkAdapter.kt
@@ -1,5 +1,6 @@
 package app.eluvio.wallet.network.adapters
 
+import android.net.Uri
 import app.eluvio.wallet.network.dto.AssetLinkDto
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Json
@@ -16,15 +17,20 @@ class AssetLinkAdapter {
         return if (assetLinkJson.slash.startsWith("/qfab/")) {
             // When the link contains "qfab", it also has a different "container" value,
             // in that case we ignore [assetLinkJson.dot.container]
-            val filePath = assetLinkJson.slash.removePrefix("/qfab/")
+            val filePath = assetLinkJson.slash.removePrefix("/qfab/").urlEncode()
             AssetLinkDto("q/$filePath")
         } else {
             val hash = assetLinkJson.dot.container
-            val filePath = assetLinkJson.slash.removePrefix("./")
+            val filePath = assetLinkJson.slash.removePrefix("./").urlEncode()
             AssetLinkDto("q/$hash/$filePath")
         }
     }
 }
+
+/**
+ * Encodes the string to be used in a URL, but leaves path separators ("/") intact.
+ */
+private fun String.urlEncode(): String = Uri.encode(this, "/")
 
 @JsonClass(generateAdapter = true)
 data class AssetLinkJson(

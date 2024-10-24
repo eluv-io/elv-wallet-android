d2095a23a01a6978ada1bb31a9c552691dd057fe
Stav Raviv <stav.raviv@eluv.io>
2024-10-24 16:07:53 -0700

    Update search filters UI

--- app/src/main/java/app/eluvio/wallet/screens/property/search/PropertySearch.kt
+++ app/src/main/java/app/eluvio/wallet/screens/property/search/PropertySearch.kt
@@ -1,9 +1,11 @@
 package app.eluvio.wallet.screens.property.search
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.BorderStroke
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,24 +23,27 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.tv.material3.Border
 import androidx.tv.material3.ClickableSurfaceDefaults
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Surface
 import androidx.tv.material3.Text
 import app.eluvio.wallet.R
+import app.eluvio.wallet.data.FabricUrl
 import app.eluvio.wallet.data.entities.v2.search.SearchFilter
 import app.eluvio.wallet.navigation.MainGraph
 import app.eluvio.wallet.screens.common.EluvioLoadingSpinner
@@ -46,7 +51,6 @@ import app.eluvio.wallet.screens.common.Overscan
 import app.eluvio.wallet.screens.common.SearchBox
 import app.eluvio.wallet.screens.common.SearchFilterChip
 import app.eluvio.wallet.screens.common.spacer
-import app.eluvio.wallet.screens.common.withAlpha
 import app.eluvio.wallet.screens.property.sections
 import app.eluvio.wallet.theme.EluvioThemePreview
 import app.eluvio.wallet.theme.carousel_36
@@ -145,7 +149,8 @@ fun FilterSelector(
         FiltersRow(
             filter = state.primaryFilter,
             selectedFilterValue = state.selectedFilters?.primaryFilterValue,
-            onClick = onPrimaryFilterClick
+            onClick = onPrimaryFilterClick,
+            label = "Filters"
         )
 
         val secondaryFilters = state.selectedFilters?.secondaryFilterAttribute
@@ -155,7 +160,6 @@ fun FilterSelector(
                 filter = secondaryFilters,
                 selectedFilterValue = state.selectedFilters.secondaryFilterValue,
                 onClick = onSecondaryFilterClick,
-                labelAlpha = 0f
             )
         }
     }
@@ -166,38 +170,38 @@ private fun FiltersRow(
     filter: SearchFilter,
     selectedFilterValue: String?,
     onClick: (SearchFilter.Value) -> Unit,
-    // For the secondary row, we still want to take up that space, but not actually show anything
-    labelAlpha: Float = 1f
+    label: String? = null
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.horizontalScroll(rememberScrollState())
     ) {
         Spacer(Modifier.width(Overscan.horizontalPadding))
-        Text("Filters", style = MaterialTheme.typography.carousel_36.withAlpha(labelAlpha))
-        filter.values.forEach { filterValue ->
+        if (label != null) {
+            Text(label, style = MaterialTheme.typography.carousel_36)
             Spacer(Modifier.width(16.dp))
+        }
+        val filterPadding = 16.dp
+        filter.values.forEach { filterValue ->
+            val interactionSource = remember { MutableInteractionSource() }
             val selected = selectedFilterValue == filterValue.value
+            val focused by interactionSource.collectIsFocusedAsState()
             if (filter.style == SearchFilter.Style.IMAGE && filterValue.imageUrl != null) {
-                val border = ClickableSurfaceDefaults.border(
-                    border = if (selected) Border(
-                        BorderStroke(2.dp, MaterialTheme.colorScheme.border)
-                    ) else Border.None
-                )
                 Surface(
                     onClick = { onClick(filterValue) },
+                    interactionSource = interactionSource,
                     colors = ClickableSurfaceDefaults.colors(
                         containerColor = Color.Transparent,
                         focusedContainerColor = Color.Transparent,
                     ),
-                    border = border,
                 ) {
                     AsyncImage(
                         model = filterValue.imageUrl,
                         contentDescription = filterValue.value,
+                        placeholder = previewPlaceholder(),
                         modifier = Modifier
+                            .alpha(if (selected || focused) 1f else 0.3f)
                             .size(56.dp)
-                            .padding(4.dp),
                     )
                 }
             } else {
@@ -208,8 +212,9 @@ private fun FiltersRow(
                     onClick = onClick,
                     onFocus = {})
             }
+            Spacer(Modifier.width(16.dp))
         }
-        Spacer(Modifier.width(Overscan.horizontalPadding))
+        Spacer(Modifier.width(Overscan.horizontalPadding - filterPadding))
     }
 }
 
@@ -220,16 +225,11 @@ private fun Header(
     onQueryChanged: (String) -> Unit,
     onSearchClicked: () -> Unit
 ) {
-    val placeholder = if (LocalInspectionMode.current) {
-        painterResource(id = R.drawable.elv_logo_bw)
-    } else {
-        null
-    }
     Row(Modifier.padding(Overscan.defaultPadding(excludeBottom = true))) {
         AsyncImage(
             model = state.headerLogo,
             contentDescription = "Logo",
-            placeholder = placeholder,
+            placeholder = previewPlaceholder(),
             modifier = Modifier
                 .height(48.dp)
                 .widthIn(max = 80.dp)
@@ -249,19 +249,46 @@ private fun Header(
 }
 
 @Composable
+private fun previewPlaceholder(@DrawableRes id: Int = R.drawable.elv_logo_bw): Painter? {
+    return if (LocalInspectionMode.current) {
+        painterResource(id = id)
+    } else {
+        null
+    }
+}
+
+@Composable
 @Preview(device = Devices.TV_720p)
 private fun PropertySearchPreview() = EluvioThemePreview {
+    val primaryFilter = SearchFilter(
+        id = "primary",
+        title = "Primary Filter",
+        values = List(4) {
+            SearchFilter.Value("Primary Filter Value $it")
+        },
+        style = SearchFilter.Style.TEXT
+    )
+    val secondaryFilter = SearchFilter(
+        id = "secondary",
+        title = "Secondary Filter",
+        values = List(4) {
+            SearchFilter.Value("Secondary Filter $it", imageUrl = object : FabricUrl {
+                override val url: String get() = "dummy"
+            })
+        },
+        style = SearchFilter.Style.IMAGE
+    )
     PropertySearch(
         PropertySearchViewModel.State(
             loading = false,
             propertyName = "FlixVerse",
-            primaryFilter = SearchFilter(
-                "p1", "Primary Filter",
-                List(4) {
-                    SearchFilter.Value("Primary Filter Value $it")
-                },
-                style = SearchFilter.Style.TEXT
-            ),
+            primaryFilter = primaryFilter,
+            selectedFilters = PropertySearchViewModel.State.SelectedFilters(
+                primaryFilter,
+                primaryFilter.values.first().value,
+                secondaryFilter,
+                secondaryFilter.values[1].value
+            )
         ),
         query = "",
         onPrimaryFilterClick = {},

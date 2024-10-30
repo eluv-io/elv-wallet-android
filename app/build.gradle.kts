789770f7a703aa827b2fdc1f62ef0f855c167eae
Stav Raviv <stav.raviv@eluv.io>
2024-10-30 12:18:58 -0700

    versioncode 24 bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "app.eluvio.wallet"
         minSdk = 23
         targetSdk = 34
-        versionCode = 23
+        versionCode = 24
         versionName = "2.0"
         // Change version name to include CI build number and version code
         project.ext

30711c304de35c0e6317c104983b455bc268a0de
Stav Raviv <stav.raviv@eluv.io>
2024-10-30 16:15:57 -0700

    Add ms perceision to RealmInstant.nowCompat

--- app/src/main/java/app/eluvio/wallet/util/realm/RealmInstantExt.kt
+++ app/src/main/java/app/eluvio/wallet/util/realm/RealmInstantExt.kt
@@ -5,6 +5,7 @@ import io.realm.kotlin.internal.RealmInstantImpl
 import io.realm.kotlin.internal.platform.currentTime
 import io.realm.kotlin.types.RealmInstant
 import java.util.Date
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.nanoseconds
 import kotlin.time.Duration.Companion.seconds
 
@@ -19,7 +20,8 @@ fun RealmInstant.Companion.nowCompat(): RealmInstant {
         val jtInstant = java.time.Clock.systemUTC().instant()
         RealmInstantImpl(jtInstant.epochSecond, jtInstant.nano)
     } else {
-        RealmInstantImpl(System.currentTimeMillis() / 1000, 0)
+        val now = System.currentTimeMillis()
+        RealmInstantImpl(now / 1000, (now % 1000).toInt() * 1_000_000)
     }
 }
 

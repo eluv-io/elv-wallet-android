4dc02c5610fce8fc5299ddc22687c6800db23031
Stav Raviv <stav.raviv@eluv.io>
2024-10-30 11:56:16 -0700

    Fix crash on API < 26
    Realm was using java.time.Clock which is only available in API 26+, so API 25 and down would crash.
    Fixed in newer realm version 1.13.0 (https://github.com/realm/realm-kotlin/blob/main/CHANGELOG.md#1130-2023-12-01 / https://github.com/realm/realm-kotlin/issues/1564)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,14 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures {
         compose = true
         buildConfig = true
